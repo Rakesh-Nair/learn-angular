de9691d544b7deb95895dd80246dfe1b03404876
Rakesh Nair <crucifyrakesh@gmail.com>
2025-09-27 20:52:11 +0530

    Submit form

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { NewTaskComponent } from './new-task/new-task.component';
+import { NewTaskData } from './task/task.model';
 
 @Component({
   selector: 'app-tasks',
@@ -55,6 +56,17 @@ onStartAddTask(){
 }
 
 onCancelAddTask(){
+  this.isAddingTask = false;
+}
+
+onSubmitAddTask(taskData:NewTaskData){
+  this.tasks.unshift({
+    id: new Date().getTime().toString(),
+    userId: this.userId,
+    title: taskData.title,
+    summary:taskData.summary,
+    dueDate: taskData.dueDate
+  });
   this.isAddingTask = false;
 }
 }
