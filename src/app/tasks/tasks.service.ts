d7a889895b3fb39abd4e13713d224235c275c6b4
Rakesh Nair <crucifyrakesh@gmail.com>
2025-10-05 19:47:44 +0530

    local storage

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -31,6 +31,13 @@ export class TasksService{
       },
     ];
 
+    constructor(){
+      const tasks = localStorage.getItem('tasks');
+      if(tasks){
+        this.tasks = JSON.parse(tasks);
+      }
+    }
+
     getTasks(id: string){
         return this.tasks.filter((task)=>task.userId === id);
     }
@@ -43,9 +50,15 @@ export class TasksService{
             summary:taskData.summary,
             dueDate: taskData.dueDate
         });
+        this.saveTasks();
     }
 
     removeTask(taskId: string){
-        this.tasks = this.tasks.filter((task) => task.id !== taskId)
+        this.tasks = this.tasks.filter((task) => task.id !== taskId);
+        this.saveTasks();
+    }
+
+    private saveTasks(){
+      localStorage.setItem('tasks', JSON.stringify(this.tasks));
     }
 }
